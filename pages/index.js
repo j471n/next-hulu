eb670417c01232c5619febc2827df712c8f488dd
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-20 15:47:52 +0530

    using API as the server side

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,41 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from "./../components/Header";
-// import Nav from "./../components/Nav";
-import Nav from '../components/Nav';
+import Head from "next/head";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>Hulu Clone</title>
-        <meta name="description" content="Hulu Clone with the help of REST API" />
+        <meta
+          name="description"
+          content="Hulu Clone with the help of REST API"
+        />
       </Head>
 
       <Header />
       <Nav />
-      {/* Results */}
+      <Results results={results} />
     </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
   )
+    .then((res) => res.json())
+    .catch((err) => console.error(err));
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
