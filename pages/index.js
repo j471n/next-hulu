aea8a70ab3b51f35598534f7e10bbf14122b32f7
Jatin Sharma (desktop) <jatinsharma089659@gmail.com>
2021-10-20 13:50:10 +0530

    Adding Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from "./../components/Header";
-// import Nav from "./../components/Nav";
-import Nav from '../components/Nav';
-
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
 export default function Home() {
   return (
     <div className="">
       <Head>
         <title>Hulu Clone</title>
-        <meta name="description" content="Hulu Clone with the help of REST API" />
+        <meta
+          name="description"
+          content="Hulu Clone with the help of REST API"
+        />
       </Head>
 
       <Header />
       <Nav />
-      {/* Results */}
+      <Results />
     </div>
-  )
+  );
+}
+
+
+
+export async function getServerSideProps(context) {
+  
+  const genre = context.query.genre;
+   
 }
